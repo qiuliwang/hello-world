6e39e7eb0cac98160f0d688353788df9a9851006
wangqiuli1012 <418674878@qq.com>
2016-12-25 16:16:27 +0800

    file modify

--- EXE1/exe1_4.cpp
+++ EXE1/exe1_4.cpp
@@ -1,8 +1,19 @@
+//Created by wangqiuli
+//418674878@qq.com
 #include <iostream>
+
+int mul(int a, int b);
 
 int main()
 {
 	int a, b;
 	std::cin >> a >> b;
 	std::cout << "answer is: " << a * b << std::endl;
+	
+	std::cout << mul(a, b) << std::endl;
+}
+
+int mul(int a, int b)
+{
+	return a * b;
 }
--- readme.txt
+++ readme.txt
@@ -1,1 +1,2 @@
 personal exercises
+git hub
